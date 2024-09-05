7381629ac7cb5811a7ee519dc1d7c22c19f9c27b
mianbao@example.com <1623666966@qq.com>
2024-09-05 20:21:20 +0800

    feat:sql拦截器自动翻译

---
+++ qh-club-subject/qh-club-infrastructure/src/main/java/com/qihang/subject/infrastructure/basic/config/MybatisConfiguration.java
@@ -0,0 +1,17 @@
+package com.qihang.subject.infrastructure.basic.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new com.qihang.subject.infrastructure.basic.config.MybatisPlusAllSqlLog());
+        return mybatisPlusInterceptor;
+    }
+
+}
