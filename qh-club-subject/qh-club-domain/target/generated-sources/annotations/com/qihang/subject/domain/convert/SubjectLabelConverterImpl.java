ff26dcf510af2bdfb9a00018f22cff22686d49a6
mianbao@example.com <1623666966@qq.com>
2024-09-08 16:28:34 +0800

    chore:maven重新打包

--- qh-club-subject/qh-club-domain/target/generated-sources/annotations/com/qihang/subject/domain/convert/SubjectLabelConverterImpl.java
+++ qh-club-subject/qh-club-domain/target/generated-sources/annotations/com/qihang/subject/domain/convert/SubjectLabelConverterImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-09-06T21:59:16+0800",
+    date = "2024-09-08T16:28:02+0800",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_291 (Oracle Corporation)"
 )
 public class SubjectLabelConverterImpl implements SubjectLabelConverter {
