bda999e621b6bf56914d5457768fe5084c6f5e8e
mianbao@example.com <1623666966@qq.com>
2024-09-11 16:59:03 +0800

    feat:oss模块的建立，minio的相关配置

--- mb-study-oss/src/main/java/com/bread/oss/util/MinioUtil.java
+++ mb-study-oss/src/main/java/com/bread/oss/util/MinioUtil.java
@@ -97,7 +97,7 @@ public class MinioUtil {
     }
 
     /**
-     * 获取文件url
+     * 获取文件urle
      */
     public String getPreviewFileUrl(String bucketName, String objectName) throws Exception {
         GetPresignedObjectUrlArgs args = GetPresignedObjectUrlArgs.builder()
